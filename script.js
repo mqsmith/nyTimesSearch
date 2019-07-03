4aacd0afbce16411e931b56cef836b8b57f3c67c
ryansims265 <ryansims265@gmail.com>
2019-07-02 20:31:39 -0400

    Created Query URL and ajax call

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 var apikey = m26NDaGVAdyor4kgdcmxEOhxjlryTF3Q;
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userinput + "&api-key=" + apikey;
+var userinput = "";
+console.log(url);
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}) .then(function (response) {
+console.log(response);
+userinput = "GA Tech";
+});
+
+
+
+
+
+
+console.log(response);
+
 
