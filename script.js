0bccabff11772514944b17f18307542d8af34505
Mike Smith <smith.mike@mac.com>
2019-07-02 20:51:56 -0400

    moved variables around to get queryURL working

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+var keyword = "boeing";
+var startYear = 1999;
+var endYear = 2019;
+
 var apiKey = "m26NDaGVAdyor4kgdcmxEOhxjlryTF3Q";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&" + startYear + "&" + endYear + "&api-key=" + apiKey;
-var keyword = "";
-var startYear
-var endYear
+
 
 console.log(queryURL);
 
@@ -11,9 +13,7 @@ $.ajax({
     method: "GET"
 }) .then(function (response) {
 console.log(response);
-keyword = "GA Tech";
-startYear = 1999;
-endYear = 2019;
+
 });
 
 
