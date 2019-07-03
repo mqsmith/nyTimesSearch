e2dbf51312597851d8b553930ebdf31a1e3eab10
Mike Smith <smith.mike@mac.com>
2019-07-02 20:38:46 -0400

    Linked jQuery

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var apikey = "m26NDaGVAdyor4kgdcmxEOhxjlryTF3Q";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userinput + "&api-key=" + apikey;
 var userinput = "";
-console.log(url);
+console.log(queryURL);
 
 $.ajax({
     url: queryURL,
