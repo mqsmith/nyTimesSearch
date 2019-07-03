ddf39b57f4d563589f8b956e23259826a44f4f3d
Mike Smith <smith.mike@mac.com>
2019-07-02 20:36:28 -0400

    added api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var apikey = m26NDaGVAdyor4kgdcmxEOhxjlryTF3Q;
+var apikey = "m26NDaGVAdyor4kgdcmxEOhxjlryTF3Q";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userinput + "&api-key=" + apikey;
 var userinput = "";
 console.log(url);
